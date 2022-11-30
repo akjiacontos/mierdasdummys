1a2afafb4c3d5f8d76147372197968599532102e
z3r0 <66015195+akjiacontos@users.noreply.github.com>
2022-11-30 02:43:34 +0100

    Add files via upload

---
+++ Calculadora Dummy.py
@@ -0,0 +1,68 @@
+import os
+
+
+
+
+numero1 = float(input("cual es tu numero?"))
+print("tu primer numero es ", numero1)
+numero2 = float(input("cual es tu segundo numero?"))
+print("tu segundo numero es", numero2)
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+
+opcion = 0
+while True:
+    print("1)Sumar cogollos 2)Restar malas vibras 3)Multiplicar momentos con Celia 4)Dividir distancias 5)Terminar Proceso")
+    opcion = int(input("Que quieres Hacer?") )
+
+    if opcion == 1:
+        print("El resultado es ",numero1+numero2)
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+
+
+    elif opcion == 2:
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print("El resultado es " ,numero1-numero2)
+
+    elif opcion == 3:
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print("El resultado es " ,numero1*numero2)
+
+    elif opcion == 4:
+         print("El resultado es " ,numero1/numero2)
+         print(" ")
+         print(" ")
+         print(" ")
+         print(" ")
+         print(" ")
+         print(" ")
+
+
+    elif opcion == 5:
+        print("Gracias por usar la calculadora de este dummy")
+        exit()
